0b23ec1a9d32b850ca009e56871a74b4806ab847
Jayson.json <jayson.h@freenet.de>
2020-04-26 19:55:55 +0200

    FPS

---
+++ Assets/Scripts/HUD/FPS.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class FPS : MonoBehaviour
+{ void Update()
+    {
+        GetComponent<TextMeshProUGUI>().text = "FPS: " + (1.0f / Time.deltaTime) + "";
+    }
+}
