67a4c6bb540cf429676e1f5c9409bce22b0101a2
Jayson.json <jayson.h@freenet.de>
2020-05-09 19:13:09 +0200

    shoot

--- Assets/Scripts/Bullet/PlayerBullet.cs
+++ Assets/Scripts/Bullet/PlayerBullet.cs
@@ -7,13 +7,27 @@ public class PlayerBullet : MonoBehaviour
 {
     public GameObject bullet;
     public GameObject bulletGroup;
+    private bool ableToShoot = true;
     void Update()
     {
         if(Input.GetKey(KeyCode.Space))
         {
-            GameObject spawned_bullet = Instantiate(bullet, bulletGroup.transform);
-            spawned_bullet.SetActive(true);
-            spawned_bullet.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 0.5f, gameObject.transform.position.z);
+            if (ableToShoot)
+            {
+                StartCoroutine(shootBullet());
+            }
         }
+    }
+    public IEnumerator shootBullet ()
+    {
+        GameObject spawned_bullet = Instantiate(bullet, bulletGroup.transform);
+        spawned_bullet.SetActive(true);
+        spawned_bullet.transform.position = new Vector3(gameObject.transform.position.x + 0.25f, gameObject.transform.position.y + 0.5f, gameObject.transform.position.z);
+        GameObject spawned_bullet1 = Instantiate(bullet, bulletGroup.transform);
+        spawned_bullet1.SetActive(true);
+        spawned_bullet1.transform.position = new Vector3(gameObject.transform.position.x - 0.25f, gameObject.transform.position.y + 0.5f, gameObject.transform.position.z);
+        ableToShoot = false;
+        yield return new WaitForSeconds(0.55f);
+        ableToShoot = true;
     }
 }
