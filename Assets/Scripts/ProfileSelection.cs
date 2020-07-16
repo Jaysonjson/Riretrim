5c9b596592d4faaae35028d28cfe05cc91411425
Jayson.json <jayson.h@freenet.de>
2020-07-16 20:59:37 +0200

    more profile

--- Assets/Scripts/ProfileSelection.cs
+++ Assets/Scripts/ProfileSelection.cs
@@ -7,19 +7,26 @@ using UnityEngine;
 public class ProfileSelection : MonoBehaviour
 {
     public GameObject ProfileObject;
+    public GameObject SlotObject;
     public GameObject[] spawnPoints;
     public GameObject currentSpawn;
     void Start()
     {
         string[] directories = Directory.GetDirectories(Application.persistentDataPath + "/profiles/");
-
+        int spawnPoint = 0;
         for (int i = 0; i < directories.Length; i++)
         {
             Profile profile = new Profile();
             profile.Data.Load(directories[i], profile);
-            GameObject profileObject = profile.Data.current ? Instantiate(ProfileObject, currentSpawn.transform) : Instantiate(ProfileObject, spawnPoints[i].transform);
+            GameObject profileObject = (profile.Data.profileName == Options.Data.CurrentProfile) ? Instantiate(ProfileObject, currentSpawn.transform) : Instantiate(ProfileObject, spawnPoints[spawnPoint].transform);
+            if (profile.Data.profileName != Options.Data.CurrentProfile) spawnPoint++;
             profileObject.GetComponent<ProfileObject>().profile = profile;
             profileObject.SetActive(true);
+        }
+        for (int i = spawnPoint; i < spawnPoints.Length; i++)
+        {
+            GameObject slotObject = Instantiate(SlotObject, spawnPoints[i].transform);
+            slotObject.SetActive(true);
         }
     }
 }
